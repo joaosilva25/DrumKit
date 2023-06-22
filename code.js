3f4941eaa8fe25924c700280bcb8afc309ccc9e3
unknown <joaosilva2505@outlook.com>
2023-06-22 17:38:22 -0300

    (primeiro commit)

---
+++ code.js
@@ -0,0 +1,9 @@
+let keyQ=document.querySelector(".keyboard_Q").addEventListener("click",() => {
+    let audioQ=document.querySelector("#key_q");
+    audioQ.play();
+})
+
+let keyW=document.querySelector(".keyboard_W").addEventListener("click",() => {
+    let audioW=document.querySelector("#key_w");
+    audioW.play();
+})
